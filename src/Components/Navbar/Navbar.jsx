df4ec01debc512ece902a0f1b530fa7c35c13c8a
Nicolas Duarte <nicollorente2002@gmail.com>
2024-08-18 20:53:05 -0300

    Cambio en css navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import "./Navbar.css"
 const Navbar = () => {
   return (
     <nav className='navbar'>
-      <div className='container'>
+      <div className='container-nav'>
         <h1>Clima Ozono</h1>
             <h2>Predicción Meteorológica</h2>
       </div>
